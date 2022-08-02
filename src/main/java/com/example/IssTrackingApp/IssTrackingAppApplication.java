273d523a5e34f97711f9bc14b0ca7f4ee4f8d26d
Rafal Bobrowski <rafal.bobrowski95@gmail.com>
2022-08-02 15:40:22 +0200

    Adding init project files.

---
+++ src/main/java/com/example/IssTrackingApp/IssTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.IssTrackingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssTrackingAppApplication.class, args);
+	}
+
+}
