948dd26a903dd0d660ed2008d7fd1d5eb8c75d66
Rafal Bobrowski <rafal.bobrowski95@gmail.com>
2022-08-04 19:34:11 +0200

    Implementations of: - Getting data about People in space every 5 sec - Saving reports to database - Added people-in-space.html with table of people - Added Navbars bo html files. - PeopleInSpaceController - Some naming refactoring

--- src/main/java/com/bobrowski/IssTrackingApp/web/controllers/ReportsController.java
+++ src/main/java/com/bobrowski/IssTrackingApp/web/controllers/ReportsController.java
@@ -1,8 +1,8 @@
 package com.bobrowski.IssTrackingApp.web.controllers;
 
-import com.bobrowski.IssTrackingApp.biz.model.ReportISS;
-import com.bobrowski.IssTrackingApp.repositories.ReportsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bobrowski.IssTrackingApp.biz.model.IssPositionReport;
+import com.bobrowski.IssTrackingApp.repositories.IssPositionReportsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/reports-position")
 public class ReportsController {
-    @Autowired
-    ReportsRepository reportsRepository;
+    private final IssPositionReportsRepository reportsRepository;
 
     @GetMapping
     public String getReports(Model model){
-        List<ReportISS> issReports = reportsRepository.findAll();
+        List<IssPositionReport> issReports = reportsRepository.findAll();
         model.addAttribute("iss_reports", issReports);
         return "reports-position";
     }
